27550601bc52af90fba7f68c7e37bff3491fbe00
xunto <hunterdelyx1@gmail.com>
2017-08-27 15:58:18 +0300

    Documentation.

--- model/character.py
+++ model/character.py
@@ -1,14 +1,29 @@
+"""
+Responsible for handling character page processes.
+"""
+
 from database import Character, User
 from model.authorization import has_permission
 
 
 class CharacterNotFound(Exception):
+    """
+    Exception is raised when no character found.
+    """
+
     def __init__(self, login):
         super().__init__("{login} не найден".format(login=login))
         self.login = login
 
 
 def get_character(login: str) -> Character:
+    """
+    Get character.
+
+    :param login: Character login.
+    :return: Character ORM object.
+    """
+
     character = Character.select(
         lambda character: character.login == login
     ).first()
@@ -19,7 +34,14 @@ def get_character(login: str) -> Character:
     return character
 
 
-def create_character(login, user: User):
+def create_character(login: str, user: User) -> Character:
+    """
+    Creates empty character.
+
+    :login: New character's login.
+    :return: New character ORM object.
+    """
+
     return Character(
         login=login,
         user=user,
@@ -27,13 +49,18 @@ def create_character(login, user: User):
     )
 
 
-def delete_character(character: Character):
+def delete_character(character: Character) -> None:
+    """
+    Deletes character.
+    :param character: Character ORM object.
+    :return: Nothing.
+    """
     character.delete()
 
 
-def can_edit_characters(user: User, author: User):
+def can_edit_characters(user: User, author: User) -> bool:
     return has_permission(user, "edit_characters") or user == author
 
 
-def can_see_characters(user: User, author: User):
+def can_see_characters(user: User, author: User) -> bool:
     return has_permission(user, "see_characters") or user == author
