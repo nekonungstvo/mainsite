dbd8e644e22819d76e2691ea4b099079ef3fb251
xunto <hunterdelyx1@gmail.com>
2017-08-29 02:16:57 +0300

    Document database objects

--- database.py
+++ database.py
@@ -1,21 +1,31 @@
+"""
+Module contains ORM objects and database connections.
+"""
+
 import datetime
 
 from flask_login import UserMixin
 from pony.orm import Database, Required, Optional, Set, sql_debug
 
-db = Database()
+db: Database = Database()
 
 db.bind('sqlite', 'database_file.sqlite', create_db=True)
 sql_debug(False)
 
 
 class News(db.Entity):
+    """
+    ORM object for site news.
+    """
     title = Required(str)
     content = Required(str)
     timestamp = Required(datetime.datetime, default=datetime.datetime.now)
 
 
 class User(db.Entity, UserMixin):
+    """
+    ORM object for registered users.
+    """
     username = Required(str, unique=True)
     password_hash = Required(str)
     about_text = Optional(str)
@@ -26,11 +36,17 @@ class User(db.Entity, UserMixin):
 
 
 class Ip(db.Entity):
+    """
+    ORM object for loging users' ip.
+    """
     ip = Required(str)
     users = Set(lambda: User)
 
 
 class Character(db.Entity):
+    """
+    ORM object for storing characters.
+    """
     login = Required(str, unique=True)
 
     name = Optional(str)
@@ -42,6 +58,9 @@ class Character(db.Entity):
 
 
 class CustomPage(db.Entity):
+    """
+    ORM object for storing custom page.
+    """
     identifier = Required(str, unique=True)
     title = Optional(str)
     content = Optional(str)
